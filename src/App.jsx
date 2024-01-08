8b24398680dc6b5b60c4e1141eb3df880d35dea1
aulafirdaus <aulafirdaus2945@gmail.com>
2024-01-08 19:13:31 +0700

    memberikan fungsi tambah dilihat dari log

--- src/App.jsx
+++ src/App.jsx
@@ -42,7 +42,12 @@ function Form(){
 
     function handleSubmit(e){
         e.preventDefault()
-        return alert(quantity)
+        if(!name) return;
+        const newItem = { name, quantity, checked: false, id: Date.now() };
+        console.log(newItem);
+
+        setName('');
+        setQuantity(1);
     }
 
     const quantityNum = [...Array(20)].map((_, i) => (
@@ -55,7 +60,7 @@ function Form(){
         <form className="add-form" onSubmit={handleSubmit}>
         <h3>Hari ini belanja apa kita?</h3>
         <div>
-            <select value={quantity} onChange={(e) => setQuantity(e.target.value)}>{quantityNum}</select>
+            <select value={quantity} onChange={(e) => setQuantity(Number(e.target.value))}>{quantityNum}</select>
             <input type="text" placeholder="nama barang..." value={name} onChange={(e) => setName(e.target.value)} />
         </div>
             <button>Tambah</button>
