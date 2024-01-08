2095043a3908b04d672ad1a26c90a87e54d7e0b6
aulafirdaus <aulafirdaus2945@gmail.com>
2024-01-08 20:17:03 +0700

    berhasil: menambah daftar item

--- src/App.jsx
+++ src/App.jsx
@@ -22,11 +22,16 @@ const groceryItems = [
 ];
 
 export default function App() {
+    const [items, setItems] = useState(groceryItems)
+
+    function handleAddItem(item) {
+        setItems([...items, item])
+    }
   return (
       <div className="app">
         <Header/>
-        <Form/>
-        <GroceryList/>
+        <Form onAddItem={handleAddItem}/>
+        <GroceryList items={items}/>
         <Footer/>
     </div>
   )
@@ -36,7 +41,7 @@ function Header(){
     return <h1>Notepadku 📝</h1>
 }
 
-function Form(){
+function Form({ onAddItem }){
     const [name, setName] = useState('')
     const [quantity, setQuantity] = useState(1)
 
@@ -44,6 +49,7 @@ function Form(){
         e.preventDefault()
         if(!name) return;
         const newItem = { name, quantity, checked: false, id: Date.now() };
+        onAddItem(newItem)
         console.log(newItem);
 
         setName('');
@@ -68,12 +74,12 @@ function Form(){
     )
 }
 
-function GroceryList(){
+function GroceryList({items}){
     return (
         <>
             <div className="list">
                 <ul>
-                    {groceryItems.map((item) => (
+                    {items.map((item) => (
                         <Item item={item} key={item.id} />
                     ))}
                 </ul>
